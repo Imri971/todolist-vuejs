f92406c041971a5e904f09a037c2661ae29e04e4
Imri <imribalourd@gmail.com>
2019-09-10 09:45:41 +0200

    first commit

---
+++ todo.js
@@ -0,0 +1,34 @@
+
+
+let app = new Vue({
+    el: '#todo',
+    data: {
+        list : " ",
+        isActive: false,
+        todos: [
+            { text: 'Apprendre JavaScript' },
+            { text: 'Apprendre Vue' },
+            { text: 'Créer quelque chose de génial' }
+          ]
+    },
+    methods: {
+        addList: function()
+        {
+            app.todos.push({ text: app.list })
+            app.list = " "
+             },
+
+        validList: function()
+        {
+            console.log(this.isActive);
+            if (this.isActive) {
+                this.isActive = !this.isActive;
+            }else
+            {
+                this.isActive = true;
+            }
+            console.log(this.isActive);
+        }
+    }
+
+})
